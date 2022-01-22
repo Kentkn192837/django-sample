00b58297dafe307dfd00c668b8109e15cc8b0bd3
Kentkn192837 <agriculture5012@gmail.com>
2022-01-23 01:06:25 +0900

    様々なクエリの渡し方のパターンを追加

--- hello/urls.py
+++ hello/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),    # views.pyのindex関数が実行される
+    path('<int:id>/<nickname>/', views.show, name='show'),
+    path('my_name_is_<nickname>.I_am_<int:age>_years_old.', views.another_pattern, name='another_pattern')
 ]
