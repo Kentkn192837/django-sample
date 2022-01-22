9b95e7b47f37bdfd650695c4a67926cb008edf52
Kentkn192837 <agriculture5012@gmail.com>
2022-01-23 00:04:05 +0900

    helloディレクトリをインクルード

--- app/urls.py
+++ app/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# 自分が追加したアプリケーションのパスをインポートする
 
 urlpatterns = [
+    # path( アクセスするアドレス, 呼び出す処理 )
     path('admin/', admin.site.urls),
+    path('hello/', include('hello.urls')),    # helloフォルダ内のurls.pyが読み込まれる
 ]
