4b5a53c60db97379dcab14cf0cca05a0f8f147fe
Kentkn192837 <agriculture5012@gmail.com>
2022-01-23 01:06:53 +0900

    渡されたクエリのパターンによって出力する処理を追加

--- hello/views.py
+++ hello/views.py
@@ -9,3 +9,12 @@ def index(request):
     else:
         result = 'please send msg parameter!'
     return HttpResponse(result)
+
+def show(request, id, nickname):
+    result = 'your id: ' + str(id) + ', name: "' \
+        + nickname + '".'
+    return HttpResponse(result)
+
+def another_pattern(request, nickname, age):
+    result = 'your account: ' + nickname + '" (' + str(age) + ').'
+    return HttpResponse(result)
